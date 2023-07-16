d0971af25a20544cfedfd1533f5033f28a56a57b
SHEFFmachine <sheff@sheffmachine.com>
2023-07-16 10:30:43 -0500

    Start divinging into threaded date server

---
+++ threaded_date_server.rb
@@ -0,0 +1,26 @@
+# require 'socket'
+# s = TCPServer.new(3939)
+# while true do
+#   conn = s.accept
+#   conn.puts 'Hi what is your name?'
+#   name = conn.gets.chomp
+#   sleep(5) # simulate a long running request here, see that the second client gets nothing back!
+#   conn.puts "Hi #{name}. Here is the date."
+#   conn.puts `date` # the backticks are sort of like an eval command
+#   conn.close
+# end
+# # s.close
+
+# Now a better server
+
+require 'socket'
+s = TCPServer.new(3939)
+while (conn = s.accept)
+  Thread.new(conn) do |connection_in_thread|
+    connection_in_thread.print "Hi. What is your name?"
+    sleep(5) # simulate a long running request here, see that the second client gets nothing back!
+    name = connection_in_thread.gets.chomp
+    connection_in_thread.puts "Hi #{name}, today's date is #{`date`}"
+    connection_in_thread.close
+  end
+end
